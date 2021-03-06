04744ebfa89432e17185d927b05d15b8bd7518ad
vzabavski <vzabavski123@gmail.com>
2021-03-06 20:47:03 +0300

    feat: add server

---
+++ backend/server.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from "cors";
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+app.use('/countries', require('./routes/app.routes'))
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://travel-app:travel-app@cluster0.sicqh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      },
+    )
+
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
