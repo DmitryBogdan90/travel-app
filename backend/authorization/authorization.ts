02a3dcb8288cc43860532cd3e755da3ebc91f81f
Misha <42773759+Gypsynkov@users.noreply.github.com>
2021-03-09 21:39:45 +0300

    feature/backend/registration
    -registration of new users was made

--- backend/authorization/authorization.ts
+++ backend/authorization/authorization.ts
@@ -1,27 +1,27 @@
-import User from '../model/User';
-import { Request, Response } from 'express';
-import { validationResult } from 'express-validator';
 import bcrypt from 'bcryptjs';
+import { Request, Response } from 'express';
+import User from '../model/User';
+import { validationResult } from 'express-validator';
 
-export class AuthController {
-  static async registration(req: Request, res: Response) {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ message: 'Registration error', errors });
-      }
-      const { username, password } = req.body;
-      const person = await User.findOne({ username });
-      if (person) {
-        res.status(400).json({ message: 'User with this name is already registered' });
-      }
-      const hashPassword = bcrypt.hashSync(password, 7);
-      const user = new User({ username, password: hashPassword });
-      await user.save();
-      res.status(400).json({ message: 'User registered successfully' });
-    } catch (e) {
-      console.log(e);
-      res.status(400).json({ message: 'Registration error' });
+
+export const registration = async (req: Request, res: Response) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: 'Registration error', errors });
     }
+    const { username, password } = req.body;
+    const person = await User.findOne({ username });
+    if (person) {
+      res.status(400).json({ message: 'User with this name is already registered' });
+    }
+    const hashPassword = bcrypt.hashSync(password, 2);
+    const user = new User({ username, password: hashPassword });
+    await user.save();
+    res.status(400).json({ message: 'User registered successfully' });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ message: 'Registration error' });
   }
 }
+
